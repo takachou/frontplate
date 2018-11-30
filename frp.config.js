3e31bf74b8cae0e4e303f3771805d4ba9fcdb9dc
takachou <5617641+takachou@users.noreply.github.com>
2018-11-30 16:15:01 +0400

    added some customization

--- frp.config.js
+++ frp.config.js
@@ -1,12 +1,13 @@
 'use strict';
 // https://github.com/frontainer/frontplate-cli/wiki/6.%E8%A8%AD%E5%AE%9A
+// https://github.com/frontainer/frontplate-cli/tree/master/config
 module.exports = function(production) {
   global.FRP_SRC = 'src';
   global.FRP_DEST = 'public';
   return {
     clean: {},
     html: {},
-    style: production ? {} : {},
+    style: production ? require('./config/style.config.production') : require('./config/style.config'),
     script: production ? {} : {},
     server: {},
     copy: {},
